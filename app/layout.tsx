7a8fae4ea436e4e803cfccb0ece23fa4baf323d6
Erik <100293508+rikjnthn@users.noreply.github.com>
2023-12-25 14:19:03 +0700

    add font and global style

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+
+import { poppins } from "@/font";
+import "../style/globals.scss";
+
+export const metadata: Metadata = {
+  title: "Tahcu",
+  description: "Tahcu is free and simple to chat",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} user-select-none`}>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+}
