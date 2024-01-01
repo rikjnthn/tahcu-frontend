ebd64daea797a0b1d064da0ea4d1cf5da3915949
Erik <100293508+rikjnthn@users.noreply.github.com>
2024-01-01 15:22:15 +0700

    create not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,34 @@
+export default function NotFound() {
+  return (
+    <div className="not-found">
+      <svg
+        width="360"
+        height="360"
+        viewBox="0 0 88 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g id="not-found-svg">
+          <path
+            id="Vector 11"
+            d="M25 28.5H2V27L17 4H19V36"
+            stroke="black"
+            strokeWidth="5"
+          />
+          <path
+            id="Vector 12"
+            d="M88 28.5H65V27L80 4H82V36"
+            stroke="black"
+            strokeWidth="5"
+          />
+          <path
+            id="Vector 13"
+            d="M54 18.0002C54 2.00024 47 2.00024 44 2.00024H43.9956C40.9932 2.00024 34 2.00023 34 18.0002C34 34.0003 41.1146 34.0002 44.0565 34.0002H44.0608C47 34.0002 54 34.0002 54 18.0002Z"
+            stroke="black"
+            strokeWidth="5"
+          />
+        </g>
+      </svg>
+    </div>
+  );
+}
