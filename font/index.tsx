8c2f4867ed837b159130def4960ddae173870bfc
Erik <100293508+rikjnthn@users.noreply.github.com>
2024-02-03 20:09:58 +0700

    add montserrat font to landing page

--- font/index.tsx
+++ font/index.tsx
@@ -1,8 +1,11 @@
-import { Poppins } from "next/font/google";
+import { Montserrat, Poppins } from "next/font/google";
 
 export const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["devanagari", "latin", "latin-ext"],
 });
 
-
+export const montserrat = Montserrat({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["cyrillic", "cyrillic-ext", "latin", "latin-ext", "vietnamese"],
+});
