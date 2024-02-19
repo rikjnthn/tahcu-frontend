c2aec65724eadf544791c6b4e762b0a0949f5d5b
Erik <100293508+rikjnthn@users.noreply.github.com>
2024-02-19 20:20:27 +0700

    add queryclientprovider wrapper and devtools in a/template file

--- app/(app)/a/template.tsx
+++ app/(app)/a/template.tsx
@@ -1,17 +1,30 @@
 "use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import dynamic from "next/dynamic";
 
 import { HomePageProvider } from "@/context/home-page-context";
 import HomePage from "@/components/home-page";
 
+const ReactQueryDevtools = dynamic(() =>
+  import("@tanstack/react-query-devtools/production").then((d) => ({
+    default: d.ReactQueryDevtools,
+  }))
+);
+
+const queryClient = new QueryClient();
+
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
-    <main className="main-page">
-      <div className="home-page-container">
-        <HomePageProvider>
-          <HomePage />
-        </HomePageProvider>
-      </div>
-      <div className="chat-page-container">{children}</div>
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <main className="main-page">
+        <div className="home-page-container">
+          <HomePageProvider>
+            <HomePage />
+          </HomePageProvider>
+        </div>
+        <div className="chat-page-container">{children}</div>
+      </main>
+    </QueryClientProvider>
   );
 }
