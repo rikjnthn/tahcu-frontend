700e4d5bab6288cae0be36c3a4b4add0b01113fc
Erik <100293508+rikjnthn@users.noreply.github.com>
2024-04-09 10:26:28 +0700

    fixed

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Viewport } from "next";
+import { redirect } from "next/navigation";
 
 import { poppins } from "@/font";
+import { isTahcuTokenVerified } from "@/action/auth";
 
 export const viewport: Viewport = {
   userScalable: false,
 };
 
-function Layout({ children }: { children: React.ReactNode }) {
+async function Layout({ children }: { children: React.ReactNode }) {
+  const isValid = await isTahcuTokenVerified();
+
+  if (!isValid) redirect("/");
   return (
     <div className={`${poppins.className} user-select-none overflow-hidden`}>
       {children}
