4a147c7f693f00162998f02cc9fc57142a2ddac4
Erik <100293508+rikjnthn@users.noreply.github.com>
2024-06-28 12:51:20 +0700

    optimized font

--- font/index.tsx
+++ font/index.tsx
@@ -1,11 +1,11 @@
 import { Montserrat, Poppins } from "next/font/google";
 
 export const poppins = Poppins({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["devanagari", "latin", "latin-ext"],
+  weight: ["200", "300", "400", "500", "600", "700"],
+  subsets: ["latin"],
 });
 
 export const montserrat = Montserrat({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["cyrillic", "cyrillic-ext", "latin", "latin-ext", "vietnamese"],
+  weight: ["200", "300", "400", "500", "600", "700"],
+  subsets: ["latin"],
 });
